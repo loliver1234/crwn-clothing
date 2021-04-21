58ce2eea24911f14d83576eb5a34d30205e389fd
O.Kostera <O.Kostera@adbglobal.com>
2021-04-21 15:26:11 +0200

    lesson 81

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompage'
+import ShopPage from './pages/shop/shop'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path ='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
