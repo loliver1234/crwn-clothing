b27b02f5fae1d477b8e2dc4c6f7cf8172ed715a9
O.Kostera <O.Kostera@adbglobal.com>
2021-04-21 15:37:11 +0200

    lesson 82

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/hompage'
-import ShopPage from './pages/shop/shop'
-
+import HomePage from './pages/homepage/hompage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path ='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
